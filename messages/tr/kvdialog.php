1a30869e1653b0244dcfa34b2e8b7fc3c7eb0fa2
Mehmet Şeref Kayalı <mskayali@yandex.com>
2017-03-11 09:53:45 +0200

    Update kvdialog.php

--- messages/tr/kvdialog.php
+++ messages/tr/kvdialog.php
@@ -17,8 +17,8 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'Cancel' => '',
-    'Confirmation' => '',
-    'Information' => '',
-    'Ok' => '',
+    'Cancel' => 'İptal',
+    'Confirmation' => 'Onay',
+    'Information' => 'Bilgi',
+    'Ok' => 'Tamam',
 ];
